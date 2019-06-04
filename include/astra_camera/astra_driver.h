7f10566fbc78070a0c65d6edf63ceee678db41bf
Dan Dedrick <dan_dedrick@jabil.com>
2019-06-04 16:05:10 -0400

    astra_driver: add desctructor to stop all streams
    This has registered callbacks with the streams so if they are not
    stopped they may still call back in with an object that has been
    destroyed, which will cause an invalid access.

--- include/astra_camera/astra_driver.h
+++ include/astra_camera/astra_driver.h
@@ -73,6 +73,7 @@ class AstraDriver
 {
 public:
   AstraDriver(ros::NodeHandle& n, ros::NodeHandle& pnh) ;
+  ~AstraDriver();
 
 private:
   typedef astra_camera::AstraConfig Config;
--- src/astra_driver.cpp
+++ src/astra_driver.cpp
@@ -186,6 +186,10 @@ AstraDriver::AstraDriver(ros::NodeHandle& n, ros::NodeHandle& pnh) :
   advertiseROSTopics();
 }
 
+AstraDriver::~AstraDriver() {
+  device_->stopAllStreams();
+}
+
 void AstraDriver::advertiseROSTopics()
 {
 
